ea3e6f1a02c54d74e1537e98ad4e3b7ac811cc76
Smeet Bhatt <smeetbhatt@Smeets-MacBook-Pro.local>
2017-10-18 17:32:56 +0530

    initial commit

---
+++ layout1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layout1
+//
+//  Created by Smeet Bhatt on 18/10/17.
+//  Copyright © 2017 Smeet Bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
